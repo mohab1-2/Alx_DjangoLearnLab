b883029f3912cf91a42c6c3964fbeb0b88220d5e
mohab ahmed zaher <mohabahmedzaher@gmail.com>
2025-08-03 19:39:58 +0300

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,11 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
 
 class BookListCreateAPIView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
@@ -17,10 +22,15 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-
-
-
-    
-    
-    
-    
+class CustomTokenObtainPairView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
+        
