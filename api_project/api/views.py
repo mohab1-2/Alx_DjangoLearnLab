3c992b7e90d8c23f8ad3dc2873030a3f6b1f812d
mohab ahmed zaher <mohabahmedzaher@gmail.com>
2025-08-03 19:25:57 +0300

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
@@ -12,3 +12,15 @@ class BookDetails(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = BookSerializer
     queryset = Book.objects.all()
+    
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+
+
+    
+    
+    
+    
