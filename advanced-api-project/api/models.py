bacc558b2102365c6f721270f4bb835dc495f22f
mohab ahmed zaher <mohabahmedzaher@gmail.com>
2025-08-10 20:12:57 +0300

    Update models.py

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # the author class in python defines a model with name attribute and string  representation
 #method 
-class Author (models.Model):
+class Author(models.Model):
     name = models.CharField(max_length=100)
     
     def __str__(self):
@@ -11,4 +11,5 @@ class Author (models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
     published_year = models.IntegerField()
