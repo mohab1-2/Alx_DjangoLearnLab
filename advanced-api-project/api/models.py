db602e6e42afcfee76f69b9b23883245e64e76a7
mohab ahmed zaher <mohabahmedzaher@gmail.com>
2025-08-10 19:51:42 +0300

    Add files via upload

---
+++ advanced-api-project/api/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# the author class in python defines a model with name attribute and string  representation
+#method 
+class Author (models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    published_year = models.IntegerField()
