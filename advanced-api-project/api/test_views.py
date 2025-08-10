ad22e4241c271169534acc367f6920b4fef35e56
mohab ahmed zaher <mohabahmedzaher@gmail.com>
2025-08-10 21:54:23 +0300

    Add files via upload

---
+++ advanced-api-project/api/test_views.py
@@ -0,0 +1,20 @@
+from rest_framework.test import APITestCase, APIClient
+from django.urls import path, include
+from django.contrib.auth.models import User
+from .models import Book
+
+def setUp(self):
+        
+        self.user = User.objects.create_user(username='testuser', password='password')
+        self.cllint =APIClient()
+        self.book_data = {
+            'title': 'Test Book',
+            'author': 'Test Author',
+            'publication_year': 2024
+        }
+        self.book = Book.objects.create(**self.book_data)
+        self.url_list = '/api/books/'
+        self.url_detail = f'/api/books/{self.book.id}/'
+        self.client.login(username='testuser', password='password')
+
+
