ccaed9b0e13741e705f7fa9fe17bcd85c5891728
mohab ahmed zaher <mohabahmedzaher@gmail.com>
2025-08-10 20:15:36 +0300

    Update serializers.py

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -8,4 +8,8 @@ class BookSerializer(serializers.ModelSerializer):
         def validate_published_year(self, value):
             if value > 2025:
                 raise serializers.ValidationError("Published year cannot be in the future")
+
             return value
+
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
