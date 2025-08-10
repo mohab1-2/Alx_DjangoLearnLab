29c4417672fcfde180b825c7dcf23be93257d575
mohab ahmed zaher <mohabahmedzaher@gmail.com>
2025-08-10 21:07:13 +0300

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,15 +1,42 @@
-from rest_framework import routers
-from .views import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.urls import path, include
- 
-router = routers.DefaultRouter()
-router.register(r'books', ListView)
-router.register(r'books/<int:pk>', DetailView, basename='book-detail')
-router.register(r'books/create', CreateView, basename='book-create')
-router.register(r'books/<int:pk>/update', UpdateView, basename='book-update')
-router.register(r'books/<int:pk>/delete', DeleteView, basename='book-delete')
-router.register(r'authors', AuthorView, basename='User')
+from django.urls import path
+from .views import (
+    BookGenericAPIView,
+    BookListView,
+    BookDetailView,
+    BookCreateView,
+    BookUpdateView,
+    BookDeleteView,
+)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', BookGenericAPIView.as_view(), name='book-generic-api'),
+    path('books/list/', BookListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
+
+
+
+
+
+
+
+
+# from rest_framework import routers
+# from .views import ListView, DetailView, CreateView, UpdateView, DeleteView
+# from django.urls import path, include
+ 
+# router = routers.DefaultRouter()
+# router.register(r'books', ListView)
+# router.register(r'books/<int:pk>', DetailView, basename='book-detail')
+# router.register(r'books/create', CreateView, basename='book-create')
+# router.register(r'books/<int:pk>/update', UpdateView, basename='book-update')
+# router.register(r'books/<int:pk>/delete', DeleteView, basename='book-delete')
+# router.register(r'authors', AuthorView, basename='User')
+
+# urlpatterns = [
+#     path('', include(router.urls)),
+# ]
+
