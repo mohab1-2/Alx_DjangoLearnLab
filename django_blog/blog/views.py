b7c445694ec533a94d3195dfbc793df238aafc02
mohab ahmed zaher <mohabahmedzaher@gmail.com>
2025-08-16 05:03:02 +0300

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,92 +1,46 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.contrib import messages
-from django.views.generic import (
-    ListView, 
-    DetailView, 
-    CreateView, 
-    UpdateView, 
-    DeleteView
-)
 from django.urls import reverse_lazy
 from .models import Post
 from .forms import PostForm
 
-# ListView - Display all blog posts
-class PostListView(ListView):
+class ListView(ListView):
     model = Post
-    template_name = 'blog/post_list.html'
+    template_name = 'blog/list.html'
     context_object_name = 'posts'
-    paginate_by = 5
-    ordering = ['-created_at']
+    ordering = ['-created_at']  # Show newest posts first
 
-    def get_queryset(self):
-        return Post.objects.filter(published=True)
-
-# DetailView - Show individual blog posts
-class PostDetailView(DetailView):
+class DetailView(DetailView):
     model = Post
-    template_name = 'blog/post_detail.html'
+    template_name = 'blog/detail.html'
     context_object_name = 'post'
 
-# CreateView - Allow authenticated users to create new posts
-class PostCreateView(LoginRequiredMixin, CreateView):
+class CreateView(LoginRequiredMixin, CreateView):
     model = Post
     form_class = PostForm
-    template_name = 'blog/post_form.html'
-    success_url = reverse_lazy('post_list')
-
+    template_name = 'blog/forms.html'
+    success_url = reverse_lazy('list')
+    
     def form_valid(self, form):
         form.instance.author = self.request.user
-        messages.success(self.request, 'Your post has been created successfully!')
         return super().form_valid(form)
 
-# UpdateView - Let authors edit their posts
-class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+class UpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     form_class = PostForm
-    template_name = 'blog/post_form.html'
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        messages.success(self.request, 'Your post has been updated successfully!')
-        return super().form_valid(form)
-
+    template_name = 'blog/forms.html'
+    success_url = reverse_lazy('list')
+    
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
 
-# DeleteView - Let authors delete their posts
-class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+class DeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    template_name = 'blog/post_confirm_delete.html'
-    success_url = reverse_lazy('post_list')
-
+    template_name = 'blog/confirm_delete.html'
+    success_url = reverse_lazy('list')
+    
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
-
-    def delete(self, request, *args, **kwargs):
-        messages.success(self.request, 'Your post has been deleted successfully!')
-        return super().delete(request, *args, **kwargs)
-
-# Function-based views for additional functionality
-@login_required
-def user_posts_view(request):
-    """Display posts by the current user"""
-    user_posts = Post.objects.filter(author=request.user).order_by('-created_at')
-    context = {
-        'posts': user_posts,
-        'title': 'My Posts'
-    }
-    return render(request, 'blog/user_posts.html', context)
-
-def home_view(request):
-    """Home page view"""
-    recent_posts = Post.objects.filter(published=True)[:3]
-    context = {
-        'posts': recent_posts,
-        'title': 'Welcome to Django Blog'
-    }
-    return render(request, 'blog/home.html', context)
