9fe03bdce8c71efaa749a3d5dd8d2da480a372a7
mohab ahmed zaher <mohabahmedzaher@gmail.com>
2025-08-16 04:46:32 +0300

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,15 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.urls import reverse
+from django.utils import timezone
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    published = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Blog Post'
+        verbose_name_plural = 'Blog Posts'
+
     def __str__(self):
         return self.title
-    
-    class Meta:
-        ordering = ['-published_date']  # Latest posts first
+
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'pk': self.pk})
+
+    def get_snippet(self):
+        """Return first 150 characters of content"""
+        return self.content[:150] + "..." if len(self.content) > 150 else self.content
+
