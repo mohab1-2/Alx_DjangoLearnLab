6b2560c1295b98591520afb10c68c70df4149c89
mohab ahmed zaher <mohabahmedzaher@gmail.com>
2025-08-18 00:26:28 +0300

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,20 +1,48 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
-from django.urls import reverse
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-class Comment(models.Model):
-    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField(help_text="Write your comment here...")
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(auto_now=True)
-    
-    class Meta:
-        ordering = ['-created_at']  # Show newest comments first
-        
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
     def __str__(self):
-        return f'Comment by {self.author.username} on {self.post.title}'
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    bio = models.TextField(blank=True)
     
-    def get_absolute_url(self):
-        return reverse('post_detail', kwargs={'pk': self.post.pk})
+    def __str__(self):
+        return f'{self.user.username} Profile'
+    
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
+post_save.connect(create_or_update_user_profile, sender=User)
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"Comments by {self.author.username} on {self.post.title}"
