19e568d5076328937bd5576ab775b0cb7dee5144
mohab ahmed zaher <mohabahmedzaher@gmail.com>
2025-08-18 00:26:14 +0300

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,21 +1,47 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile, Post, Comment
+from taggit.forms import TagField
 
-class CustomUserCreationForm(UserCreationForm):
+
+class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    first_name = forms.CharField(max_length=30, required=False)
-    last_name = forms.CharField(max_length=30, required=False)
 
     class Meta:
         model = User
-        fields = ("username", "email", "first_name", "last_name", "password1", "password2")
+        fields = ['username', 'email', 'password1', 'password']
 
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.email = self.cleaned_data["email"]
-        user.first_name = self.cleaned_data["first_name"]
-        user.last_name = self.cleaned_data["last_name"]
-        if commit:
-            user.save()
-        return user
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image', 'bio']
+
+class TagWidget(forms.TextInput):
+    def __init__(self, attrs=None):
+        final_attrs = {'class': 'tag-widget', 'placeholder': 'Add tags...'}
+        if attrs:
+            final_attrs.update(attrs)
+        super().__init__(attrs=final_attrs)
+
+class PostForm(forms.ModelForm):
+    tags = TagField(
+        widget=TagWidget(),
+        required=False,
+    )
+
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your post title'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write you content here ...'})
+        }
+
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
