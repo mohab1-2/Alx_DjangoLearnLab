6538f9ccc4278b5415b4b16dcf1b82654b6a2de9
mohab ahmed zaher <mohabahmedzaher@gmail.com>
2025-08-24 23:13:20 +0300

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,19 +1,39 @@
-from django.db import models
+# accounts/models.py
 from django.contrib.auth.models import AbstractUser
-from django.conf import settings
+from django.db import models
 
-class CustomUser(AbstractUser):
+class User(AbstractUser):
     bio = models.TextField(max_length=500, blank=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+    
+    # Many-to-many relationship for following
+    following = models.ManyToManyField(
+        'self', 
+        through='Follow',
+        related_name='followers',
+        symmetrical=False,
+        blank=True
+    )
     
     def __str__(self):
         return self.username
+
+class Follow(models.Model):
+    """Intermediate model for user follows"""
+    follower = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name='user_following'
+    )
+    following = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name='user_followers'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
     
-    @property
-    def followers_count(self):
-        return self.followers.count()
-    
-    @property
-    def following_count(self):
-        return self.following.count()
+    class Meta:
+        unique_together = ('follower', 'following')
+        
+    def __str__(self):
+        return f"{self.follower.username} follows {self.following.username}"
